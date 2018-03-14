0e921ac7f9ebd356935e86aa20810a4f9c407356
Peter Chung <hkpeterpeter@gmail.com>
2018-03-14 17:57:25 +0800

    Added basic gulp workflow

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+
+
+gulp.task('minify', function() {
+    return gulp.src('public/**/*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('public'));
+});
+
+gulp.task('default', ['minify']);
