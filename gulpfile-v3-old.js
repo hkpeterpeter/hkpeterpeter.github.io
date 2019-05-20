e04b12d82f1e4812005b7225a732fc57d628ded7
Peter Chung <hkpeterpeter@gmail.com>
2019-05-20 10:04:36 +0800

    Fix Gulp from v3 to v4

--- gulpfile-v3-old.js
+++ gulpfile-v3-old.js
@@ -1,13 +1,11 @@
 var gulp = require('gulp');
 var htmlmin = require('gulp-htmlmin');
 
+
 gulp.task('minify', function() {
     return gulp.src('public/**/*.html')
         .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('public'));
 });
 
-// For gulp v4, series and parallel are new APIs introduced
-gulp.task('default', gulp.series('minify', (done) => {
-    done();
-}));
+gulp.task('default', ['minify']);
